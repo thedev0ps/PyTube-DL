419d72f437378142c2bad7018431e62c87bf290b
Adam Al Shouli <adamalshouli05@gmail.com>
2025-10-31 16:33:27 +0300

    feat(utils): add get_default_paths function

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,9 @@
 import time
+import json
+from pathlib import Path
+
+parent_dir = Path(__file__).parent.parent
+config_path = parent_dir / "config.json"
 
 
 def type_out(string: str, delay: float = 0.1):
@@ -13,3 +18,21 @@ def type_out(string: str, delay: float = 0.1):
         print(char, end="", flush=True)
         time.sleep(delay)
     print()
+
+
+def get_default_paths(config_path: Path = Path("config.json")) -> dict:
+    """
+    Returns default download paths for video and audio from the config file.
+
+    Args:
+        config_path (Path, optional): Path to the config JSON file. Defaults to 'config.json'.
+
+    Returns:
+        dict: {"video": <video_path>, "audio": <audio_path>}
+    """
+    with open(config_path, "r") as config_file:
+        config = json.load(config_file)
+    return {
+        "video": config["default-video-path"],
+        "audio": config["default-audio-path"],
+    }
