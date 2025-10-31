12cb69745ca1a62780bc5814dc904ac3c90858de
Adam Al Shouli <adamalshouli05@gmail.com>
2025-10-31 16:29:59 +0300

    feat(pytube): add download_video function

--- app/pytube.py
+++ app/pytube.py
@@ -1,6 +1,9 @@
 import yt_dlp
+import utils
+import os
 
-ydl = yt_dlp.YoutubeDL({"quiet": True, "no_warnings": True})
+options = {"quiet": True, "no_warnings": True}
+ydl = yt_dlp.YoutubeDL(options)
 
 
 def get_video_info(url: str) -> dict | None:
@@ -21,5 +24,30 @@ def get_video_info(url: str) -> dict | None:
             "uploader": info.get("uploader"),
             "views": info.get("view_count"),
         }
+    except yt_dlp.utils.DownloadError:
+        return None
+
+
+def download_video(url: str) -> None:
+    """
+    Download a video from a URL to the default video folder using yt-dlp.
+
+    The downloaded video will be saved with the filename format:
+    "<title> - <uploader>.<ext>".
+
+    Args:
+        url (str): The URL of the video to download.
+
+    Returns:
+        None: This function does not return anything. If the download fails,
+              it silently returns None.
+    """
+    try:
+        output_folder = utils.get_default_paths().get("video")
+        outtmpl = os.path.join(output_folder, "%(title)s - %(uploader)s.%(ext)s")
+        local_options = options.copy()
+        local_options.update({"outtmpl": outtmpl})
+        with yt_dlp.YoutubeDL(local_options) as ydl:
+            ydl.download([url])
     except yt_dlp.utils.DownloadError:
         return None
